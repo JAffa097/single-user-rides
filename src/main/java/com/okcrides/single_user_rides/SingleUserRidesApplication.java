31308e2653a3b575ac4f2ec58c2459acac841d05
JAffa097 <jpgpgma666@gmail.com>
2024-07-08 01:24:55 +0530

    initial commit

---
+++ src/main/java/com/okcrides/single_user_rides/SingleUserRidesApplication.java
@@ -0,0 +1,17 @@
+package com.okcrides.single_user_rides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class SingleUserRidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleUserRidesApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/okcrides/single_user_rides/SingleUserRidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okcrides.single_user_rides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleUserRidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
